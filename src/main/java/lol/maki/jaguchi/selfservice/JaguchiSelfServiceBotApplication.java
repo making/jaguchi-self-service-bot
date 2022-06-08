be0408dd7cfd8ee3efaabcb053c3288b1d7781f0
Toshiaki Maki <tmaki@vmware.com>
2022-06-09 02:31:00 +0900

    Remove HttpTraceRepository

--- src/main/java/lol/maki/jaguchi/selfservice/JaguchiSelfServiceBotApplication.java
+++ src/main/java/lol/maki/jaguchi/selfservice/JaguchiSelfServiceBotApplication.java
@@ -9,8 +9,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
-import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -18,11 +16,6 @@ public class JaguchiSelfServiceBotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JaguchiSelfServiceBotApplication.class, args);
-	}
-
-	@Bean
-	public HttpTraceRepository htttpTraceRepository() {
-		return new InMemoryHttpTraceRepository();
 	}
 
 	@Bean
